8a90cec81b90ae89d1383233785c94e11521ab8a
Víctor Martínez <40225996+vmartinez1984@users.noreply.github.com>
2025-06-25 20:34:08 -0600

    Update config.py

--- repositories/config.py
+++ repositories/config.py
@@ -1,2 +1,1 @@
-# uri = "mongodb://root:123456@localhost:27017/?authMechanism=DEFAULT"
-uri = "mongodb+srv://superiorviktor:4hzYZt1lSTcBCycU@cluster0.53l4ojb.mongodb.net/"
+uri = "mongodb://root:123456@localhost:27017/?authMechanism=DEFAULT"
